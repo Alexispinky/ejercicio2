df06c11b8d6336e40e890abd8419b0e2fc45a7f5
Alexispinky <alejandroispinky@hotmail.com>
2022-03-18 20:17:58 -0600

    Initial commit

---
+++ ejercicio2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejercicio2
+//
+//  Created by DISMOV on 18/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
